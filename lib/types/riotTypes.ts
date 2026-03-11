5cb8ebd989565e3b9deb583d69c3a408b5e366e4
Alex Guo <alex.guo@student.ap.be>
2026-03-11 17:59:38 +0100

    refactor: update RiotAccountSchema to use string type for puuid and make gameName and tagLine optional

--- lib/types/riotTypes.ts
+++ lib/types/riotTypes.ts
@@ -1,10 +1,20 @@
 import { z } from "zod";
 
 export const RiotAccountSchema = z.object({
-    puuid: z.uuid(),
-    gameName: z.string().max(16),
-    tagLine: z.string().max(5),
+  puuid: z.string().length(78),
+  gameName: z.string().max(16).optional(),
+  tagLine: z.string().max(5).optional(),
 });
 
-
 export type RiotAccount = z.infer<typeof RiotAccountSchema>;
+
+export const RiotSummonerSchema = z.object({
+  accountId: z.string(),
+  id: z.string(),
+  puuid: z.string().length(78),
+  profileIconId: z.number(),
+  revisionDate: z.number(),
+  summonerLevel: z.number(),
+});
+
+export type RiotSummoner = z.infer<typeof RiotSummonerSchema>;
